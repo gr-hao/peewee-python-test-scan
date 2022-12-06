166f8b2a357f8eba56201794fa67aefb8397b4a8
gr-hao <106646502+gr-hao@users.noreply.github.com>
2022-12-06 21:37:19 +0700

    Update bench.py

--- bench.py
+++ bench.py
@@ -1,6 +1,8 @@
 from peewee import *
 
-
+ivate_key = "xxx"
+privateKey = "sdfsd"
+    
 db = SqliteDatabase(':memory:')
 #db = PostgresqlDatabase('peewee_test', host='127.0.0.1', port=26257, user='root')
 #db = PostgresqlDatabase('peewee_test', host='127.0.0.1', user='postgres')
