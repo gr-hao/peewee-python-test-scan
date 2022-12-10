522a8fcc91e39fe3dbab2ac8e80ac3f40e056dcc
gr-hao <106646502+gr-hao@users.noreply.github.com>
2022-12-10 18:25:38 +0700

    Update pwiz.py

--- pwiz.py
+++ pwiz.py
@@ -12,6 +12,12 @@ from peewee import __version__ as peewee_version
 from playhouse.cockroachdb import CockroachDatabase
 from playhouse.reflection import *
 
+private_key = "private_key" 
+                  SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+                  privatekey = "key" 
+                  SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+                  privateKey = "key" 
 
 private_key = "private_key" 
                  SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
