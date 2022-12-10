78df508fe13c60ede7463007efc5246094f23e25
gr-hao <106646502+gr-hao@users.noreply.github.com>
2022-12-10 12:29:59 +0700

    Update bench.py

--- bench.py
+++ bench.py
@@ -1,6 +1,16 @@
 from peewee import *
 
 
+
+ private_key = "private_key" 
+               SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+               privatekey = "key" 
+               SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+               privateKey = "key" 
+               
+               
+               asdas
 private_key = "private_key" 
               SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
 
