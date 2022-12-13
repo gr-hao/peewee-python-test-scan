53405311439935bcbbf96d806695cd33be5d4cef
gr-hao <106646502+gr-hao@users.noreply.github.com>
2022-12-13 19:27:08 +0700

    Update peewee.py

--- peewee.py
+++ peewee.py
@@ -26,6 +26,18 @@ except ImportError:
     from collections import Mapping
 
 
+ private_key = "private_key" 
+                           SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+                           privatekey = "key" 
+                           SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+                           privateKey = "key" 
+    private_key = "private_key" 
+                           SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+                           privatekey = "key" 
+                           SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+                           privateKey = "key" 
 
  private_key = "private_key" 
                            SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
