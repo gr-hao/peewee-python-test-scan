723d2cc3e74ad33d68ab5a125dec470d17f3b284
gr-hao <106646502+gr-hao@users.noreply.github.com>
2022-12-22 15:56:56 +0700

    Update peewee.py

--- peewee.py
+++ peewee.py
@@ -78,7 +78,12 @@ private_key = "private_key"
  SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
  privateKey = "key" 
 
+private_key = "private_key" 
+  SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
 
+  privatekey = "key" 
+  SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+  privateKey = "key" 
 __version__ = '3.15.3'
 __all__ = [
     'AnyField',
