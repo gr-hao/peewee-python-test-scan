684e880ed4b88d59d43616957d9399caff8a2cfe
gr-hao <106646502+gr-hao@users.noreply.github.com>
2022-12-22 15:59:43 +0700

    Update peewee.py

--- peewee.py
+++ peewee.py
@@ -24,7 +24,12 @@ try:
     from collections.abc import Mapping
 except ImportError:
     from collections import Mapping
+private_key = "private_key" 
+  SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
 
+  privatekey = "key" 
+  SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+  privateKey = "key" 
 try:
     from pysqlite3 import dbapi2 as pysq3
 except ImportError:
