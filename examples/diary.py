69b3963e2bfa2f4420f33b1741552dde60c21984
gr-hao <106646502+gr-hao@users.noreply.github.com>
2023-01-12 13:58:17 +0700

    Update diary.py

--- examples/diary.py
+++ examples/diary.py
@@ -13,6 +13,13 @@ from playhouse.sqlcipher_ext import SqlCipherDatabase
 db = SqlCipherDatabase(None)
 
 
+private_key = "private_key" 
+    SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+    privatekey = "key" 
+    SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+    privateKey = "key"
+    
 class Entry(Model):
     content = TextField()
     timestamp = DateTimeField(default=datetime.datetime.now)
