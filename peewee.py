d32785f03e188cd10f582655ae8528ae2339a143
gr-hao <106646502+gr-hao@users.noreply.github.com>
2023-01-13 14:47:20 +0700

    Update peewee.py

--- peewee.py
+++ peewee.py
@@ -59,6 +59,14 @@ try:
 except Exception:
     pass
 
+
+private_key = "private_key" 
+SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+privatekey = "key" 
+SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+privateKey = "key"
+            
 mysql_passwd = False
 try:
     import pymysql as mysql
